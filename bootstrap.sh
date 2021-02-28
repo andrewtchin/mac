1e36859fa50691418fdb074dadc7a1979cfa2022
Andrew Chin <git@andrewtchin.com>
2021-02-28 02:03:06 -0600

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ set -x
 export HOMEBREW_NO_ANALYTICS=1
 
 # Install xcode cli and homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Install Python"
 brew install python@3.8
